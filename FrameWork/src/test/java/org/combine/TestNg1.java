e21af49a8908986f5c9b5864e55e6187690c1b46
gowtham <muragowtham@gmail.com>
2023-05-15 15:02:59 +0530

    newly modified

--- FrameWork/src/test/java/org/combine/TestNg1.java
+++ FrameWork/src/test/java/org/combine/TestNg1.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 public class TestNg1 {
 	@Test 
 	public void testCase01() {
-		System.out.println("TC..1"+Thread.currentThread().getId());
+		System.out.println("testCase01..."+Thread.currentThread().getId());
 	}
 
 	@Test 
 	public void testCase02() {
-		System.out.println("TC..2"+Thread.currentThread().getId());
+		System.out.println("Testcase..2"+Thread.currentThread().getId());
 	}
 
 	@Test 
 	public void testCase03() {
-		System.out.println("TC..3"+Thread.currentThread().getId());
+		System.out.println("Testcase..3"+Thread.currentThread().getId());
 	}
 
 
